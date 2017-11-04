6e20c912788389be19d9fc3ca9292740805e322d
Dan Schultzer <dan@dreamconception.com>
2017-11-03 21:59:19 -0700

    Refactor code
    Added tests too

--- lib/coherence_assent/strategies/basecamp.ex
+++ lib/coherence_assent/strategies/basecamp.ex
@@ -6,29 +6,16 @@ defmodule CoherenceAssent.Strategy.Basecamp do
   alias CoherenceAssent.Strategy.Helpers
   alias CoherenceAssent.Strategies.OAuth2, as: OAuth2Helper
 
-  def authorize_url(conn: conn, config: config) do
-    config = config |> set_config
-    OAuth2Helper.authorize_url(conn: conn, config: config)
+  def authorize_url(conn, config) do
+    OAuth2Helper.authorize_url(conn, set_config(config))
   end
 
-  def callback(conn: conn, config: config, params: params) do
-    config = config |> set_config
-    client = config |> OAuth2.Client.new()
+  def callback(conn, config, params) do
+    config = set_config(config)
 
-    {:ok, %{conn: conn, client: client}}
-    |> OAuth2Helper.check_conn(params)
-    {:ok, client} = client
-                    |> OAuth2.Client.get_token(
-                      code: params["code"],
-                      client_secret: config[:client_secret],
-                      redirect_uri: params["redirect_uri"],
-                      type: config[:authorization_params][:type]
-                    )
-    client
-    |> OAuth2.Client.get(config[:user_url] || raise "No user URL set")
-    |> process_user_response(conn, client)
-    |> normalize()
-
+    conn
+    |> OAuth2Helper.callback(config, params)
+    |> normalize
   end
 
   defp set_config(config) do
@@ -43,15 +30,6 @@ defmodule CoherenceAssent.Strategy.Basecamp do
     |> Keyword.put(:strategy, OAuth2.Strategy.AuthCode)
   end
 
-  defp process_user_response({:ok, %OAuth2.Response{body: user}}, conn, client),
-    do: {:ok, %{conn: conn, client: client, user: user}}
-  defp process_user_response({:error, %OAuth2.Response{status_code: 401, body: _body}}, _conn, _client) do
-    raise "Unauthorized token"
-  end
-  defp process_user_response({:error, %OAuth2.Error{reason: reason}}, _conn, _client) do
-    raise "Error: #{inspect reason}"
-  end
-
   defp normalize({:ok, %{conn: conn, client: client, user: user}}) do
     user =
       %{"uid"         => Integer.to_string(user["identity"]["id"]),
@@ -59,8 +37,7 @@ defmodule CoherenceAssent.Strategy.Basecamp do
         "first_name"  => user["identity"]["first_name"],
         "last_name"   => user["identity"]["last_name"],
         "email"       => user["identity"]["email_address"],
-        "accounts"    => user["accounts"],
-        "token"       => client.token |> Map.from_struct() |> Map.take([:access_token, :expires_at, :refresh_token])}
+        "accounts"    => user["accounts"]}
         |> Helpers.prune
     {:ok, %{conn: conn, client: client, user: user}}
   end
