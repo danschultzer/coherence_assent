20b082c290d2ecbea4255dfd20307f4d03e34e63
Dan Schultzer <dan@dreamconception.com>
2018-02-12 16:53:35 -0800

    Refactor user session creation

--- lib/coherence_assent/web/controllers/controller.ex
+++ lib/coherence_assent/web/controllers/controller.ex
@@ -8,12 +8,12 @@ defmodule CoherenceAssent.Controller do
   def callback_response({:ok, :user_created, user}, conn, _provider, _user_params, params) do
     conn
     |> send_confirmation(user)
-    |> Coherence.ControllerHelpers.login_user(user)
+    |> create_user_session(user)
     |> redirect_to(:registration_create, params)
   end
   def callback_response({:ok, _type, user}, conn, _provider, _user_params, params) do
     conn
-    |> Coherence.ControllerHelpers.login_user(user)
+    |> create_user_session(user)
     |> redirect_to(:session_create, params)
   end
   def callback_response({:error, :bound_to_different_user}, conn, provider, _user_params, _params) do
@@ -50,5 +50,9 @@ defmodule CoherenceAssent.Controller do
       false -> Coherence.ControllerHelpers.send_confirmation(conn, user, Coherence.Config.user_schema)
       _     -> conn
     end
+  end
+
+  defp create_user_session(conn, user) do
+    Coherence.ControllerHelpers.login_user(conn, user)
   end
 end
